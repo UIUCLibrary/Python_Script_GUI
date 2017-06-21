3bf14c50226fd5b833702045b66bbdb151952123
hborcher <hborcher@illinois.edu>
2017-06-21 16:20:14 -0500

    added start abstract method, abort, is_running, and announced_ended

--- script_gui/abs_script.py
+++ script_gui/abs_script.py
@@ -1,9 +1,13 @@
 import abc
 import logging
 import threading
+from PyQt5.QtCore import pyqtSignal, QObject
 
 
-class AbsScript(metaclass=abc.ABCMeta):
+
+class AbsScript(QObject):
+    halted_signal = pyqtSignal()
+
     @property
     @abc.abstractmethod
     def title(self) -> str:
@@ -14,5 +18,22 @@ class AbsScript(metaclass=abc.ABCMeta):
         pass
 
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
+        super().__init__()
         self.t = threading.Thread()
+        self._abort_flag = threading.Event()
+        self.logger = logging.getLogger(self.__module__)
+
+    @abc.abstractmethod
+    def start(self):
+        pass
+
+    def abort(self):
+        self.logger.debug("Setting abort flag")
+        self._abort_flag.set()
+        self.logger.debug("Abort flag has been set")
+
+    def is_running(self) -> bool:
+        return self.t.is_alive()
+
+    def announce_ended(self):
+        self.halted_signal.emit()
