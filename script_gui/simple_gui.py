876b70f740c411fe438f0b6657c0c16d1b062ec2
hborcher <hborcher@illinois.edu>
2017-07-07 13:45:33 -0500

    added a exception handler

--- script_gui/simple_gui.py
+++ script_gui/simple_gui.py
@@ -10,7 +10,13 @@ from . import gui
 from . import gui_logger
 from . import states
 
-
+def catch_exceptions(t, val, tb):
+    print("EXCEPTION")
+    QtWidgets.QMessageBox.critical(None,
+                                   "An exception was raised",
+                                   "Exception type: {}".format(t))
+old_hook = sys.excepthook
+sys.excepthook = catch_exceptions
 
 class SimpleGui(QtWidgets.QMainWindow, gui.Ui_MainWindow):
     abort_signal = pyqtSignal()
@@ -23,6 +29,17 @@ class SimpleGui(QtWidgets.QMainWindow, gui.Ui_MainWindow):
         self.abort_signal.connect(self.script.abort)
         self.script.halted_signal.connect(lambda: self.current_state.confirm_finished())
         self.setupUi(self)
+        self.arg_input = dict()
+
+        if len(self.script.args._required) > 0:
+            self.formLayout.addRow((QtWidgets.QLabel("Required Arguments:")))
+            for arg_name, arg_value in self.script.args._required.items():
+                new_line_edit = QtWidgets.QLineEdit()
+                new_line_edit.setText(arg_value.value)
+                new_line_edit.textChanged.connect(lambda d, arg=arg_name: self._set_arg(arg, d))
+                self.arg_input[arg_name] = new_line_edit
+                self.formLayout.addRow((QtWidgets.QLabel(arg_name)), new_line_edit)
+
         self.startButton.clicked.connect(self.process)
         self.stopButton.clicked.connect(self.abort)
         self.setWindowTitle(self.script.title)
@@ -62,3 +79,6 @@ class SimpleGui(QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
     def _execute(self):
         self.script.start()
+
+    def _set_arg(self, name, value):
+        self.script.args[name].value = value
