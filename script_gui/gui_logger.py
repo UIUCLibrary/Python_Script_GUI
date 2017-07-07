1fe0f1472e0455d90d37bb80204b5879b7f5900f
hborcher <hborcher@illinois.edu>
2017-07-07 15:13:43 -0500

    removed debug print

--- script_gui/gui_logger.py
+++ script_gui/gui_logger.py
@@ -1,6 +1,6 @@
 import logging
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 
 
 class QtLogger(logging.Handler):
@@ -9,22 +9,13 @@ class QtLogger(logging.Handler):
         self.widget = widget
 
     def emit(self, record):
-        print("emitting")
-
-        print("16")
         msg = self.format(record)
-        print("18")
-        print(msg)
-        print(self.widget)
         self.widget.append(str(msg))
-        print("21")
+        QtGui.QGuiApplication.processEvents()
+
         sb = self.widget.verticalScrollBar()
-        print("23")
 
         # keep text scrolled at the bottom as it updates if it's already at the bottom
         diff = sb.maximum() - sb.value()
-        print("27")
         if diff < 20:
             sb.setValue(sb.maximum())
-        print("30")
-        print("emitted")
