990958c225452da70b2343bc73281cfc75cbbb1f
hborcher <hborcher@illinois.edu>
2017-07-07 15:57:56 -0500

    added validate and help

--- script_gui/args.py
+++ script_gui/args.py
@@ -1,12 +1,14 @@
 import collections.abc
 import collections
+
 import itertools
 
 
 class ScriptArgument:
-    def __init__(self, value):
+    def __init__(self, value, help=None):
         self.value = value
         self._validation = lambda v: True
+        self.help = help
 
     def __str__(self):
         return self.value
@@ -17,6 +19,9 @@ class ScriptArgument:
     @property
     def valid(self):
         return self._validation(self.value)
+
+    def check_value(self, value) -> bool:
+        return self._validation(value)
 
 
 class Arguments(collections.abc.Mapping):
@@ -30,16 +35,19 @@ class Arguments(collections.abc.Mapping):
     def __iter__(self):
         return itertools.chain(self._required.__iter__(), self._optional.__iter__())
 
-    # TODO: add optional validation callback
     def add_required(self, name, default="", *args, **kwargs):
+        new_arg = ScriptArgument(default)
+
         if "validate" in kwargs:
             validator = lambda foo: foo != "" and kwargs["validate"](foo)
         else:
             validator = lambda foo: foo != ""
 
+        if "help" in kwargs:
+            new_arg.help = kwargs['help']
         if name in self._required or name in self._optional:
             raise KeyError("{} already being used.")
-        new_arg = ScriptArgument(default)
+
         new_arg.set_validator(validator)
         self._required[name] = new_arg
 
@@ -68,7 +76,6 @@ class Arguments(collections.abc.Mapping):
             self._optional[key] = item
         else:
             raise KeyError
-
 
     @property
     def missing(self):
