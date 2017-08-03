5c9d2b9dd7e09143b32dc5db3a867ed5b3485f40
Henry Borchers <henryborchers@yahoo.com>
2017-08-03 16:38:06 -0500

    removed simple Gui version 1

--- script_gui/__init__.py
+++ script_gui/__init__.py
@@ -1,4 +1,4 @@
 from .__version__ import  __version__, __author__, __author_email__, __description__, __title__, __url__
 from . import gui_logger
-from .simple_gui import SimpleGui
+# from .simple_gui import SimpleGui
 from .abs_script import AbsScript
