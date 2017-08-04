dd0e2181be9b9573b900105f037b3393e6469b84
hborcher <hborcher@illinois.edu>
2017-08-04 16:20:05 -0500

    added stated

--- script_gui/script_runners/abs_script_runner.py
+++ script_gui/script_runners/abs_script_runner.py
@@ -5,6 +5,8 @@ import sys
 from PyQt5.QtCore import pyqtSignal, QObject
 from script_gui.script_signals import SignalTypes
 # from script_gui.abs_script import AbsScript2
+from script_gui import abs_script
+from . import states
 import threading
 
 
@@ -21,15 +23,44 @@ class NoSignals:
     CHANGE = NoOpSignal()
 
 
-class ScriptRunner(metaclass=abc.ABCMeta):
+class JobThread(threading.Thread):
+    def __init__(self, runner=None, callback=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.runner = runner
+        self.callback = callback
+
+    def run(self):
+        self.runner.script.run()
+        if self.callback:
+            self.callback()
+
+
+class absScriptRunner(metaclass=abc.ABCMeta):
     signals = NoSignals()
 
-    def __init__(self, script: "AbsScript2"):
+    def __init__(self, script: "abs_script.AbsScript2"):
         self.logger = self.configure_logger()
-        self._thread_runner = threading.Thread()
+        self._thread_runner = JobThread()
         self._abort_flag = threading.Event()
         self.script = script
         self.script.set_abort_flag(self._abort_flag)
+        self.all_states = {
+            "idle": states.IdleState(self),
+            "running": states.RunningState(self),
+            "halting": states.HaltingState(self),
+            "completed": states.CompletedState(self),
+            "failed": states.FailedState(self),
+        }
+        self.current_state = self.all_states["idle"]
+
+    def change_state(self, state_name: str):
+        self.current_state.teardown()
+        try:
+            self.current_state = self.all_states[state_name]
+        except KeyError:
+            print("Invalid state {}".format(state_name))
+            raise KeyError("Invalid state {}".format(state_name))
+        self.current_state.setup()
 
     @property
     def title(self) -> str:
@@ -39,9 +70,24 @@ class ScriptRunner(metaclass=abc.ABCMeta):
         return self.script
 
     def start(self, daemon=False):
+        self.current_state.start(daemon)
+
+    def exec(self, daemon: bool):
+        """Execute the script, either threaded or not threaded.
+
+        Notes:
+            This should not be used directly to execute the script. To make sure that states are followed,
+            use the start method instead.
+
+        Args:
+            daemon: Run script blocked or on a separate thread.
+
+
+        """
         self.logger.debug("Starting script")
         if daemon:
-            self._thread_runner = threading.Thread(target=self.script.run)
+            self._thread_runner = JobThread(self)
+            # self._thread_runner.runner = self
             self._thread_runner.daemon = True
             self._thread_runner.start()
         else:
@@ -51,12 +97,12 @@ class ScriptRunner(metaclass=abc.ABCMeta):
         return self._thread_runner.is_alive()
 
     def abort(self, reason=None):
+        self.current_state.abort(reason)
         # TODO set up abort reason
-        self._abort_flag.set()
 
     def reset(self):
-        self._abort_flag.clear()
+        self.current_state.reset()
 
     @abc.abstractmethod
-    def configure_logger(self,level=logging.INFO):
+    def configure_logger(self, level=logging.INFO):
         pass
