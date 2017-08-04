c92219a26909704e9c1ed97e778c09f14a85798e
hborcher <hborcher@illinois.edu>
2017-08-04 16:20:23 -0500

    updated name

--- script_gui/script_runners/cli_runner.py
+++ script_gui/script_runners/cli_runner.py
@@ -1,6 +1,7 @@
 from script_gui.script_runners import abs_script_runner
 import logging
 
-class CLIScriptRunner(abs_script_runner.ScriptRunner):
-    def configure_logger(self,level=logging.INFO):
+
+class CLIScriptRunner(abs_script_runner.absScriptRunner):
+    def configure_logger(self, level=logging.INFO):
         return logging.getLogger(__name__)
